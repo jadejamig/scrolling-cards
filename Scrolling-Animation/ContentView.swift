6d16c87fc2c5bf4adb8ea66705f0249f57e63813
Jade Jamig <ajamig016@gmail.com>
2021-05-30 08:49:57 +0800

    Initial Commit

---
+++ Scrolling-Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrolling-Animation
+//
+//  Created by Anselm Jade Jamig on 5/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scrolling-Animation/Scrolling_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrolling_AnimationApp.swift
+//  Scrolling-Animation
+//
+//  Created by Anselm Jade Jamig on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Scrolling_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
